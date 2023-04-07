709a4914492114dfebd99da110d8edd0db649ae8
evan <evan@example.com>
2023-04-07 11:21:46 +0800

    Initial commit

---
+++ app/src/main/java/com/kotlin/JsonTestKotlin.kt
@@ -0,0 +1,16 @@
+package com.kotlin
+
+/**
+ * 单独的kt文件运行必须要新建一个kotlin项目再新建kt文件才能运行，在java项目中无法运行
+ */
+class JsonTestKotlin {
+
+    companion object {
+        @JvmStatic
+        fun main(args: Array<String>) {
+            println("Hello!")
+        }
+    }
+}
+
+
