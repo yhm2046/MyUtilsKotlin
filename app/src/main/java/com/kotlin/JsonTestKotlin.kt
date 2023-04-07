884c29a56642724a995337d1bd1b786a22b86851
evan <evan@example.com>
2023-04-07 14:10:47 +0800

    添加json解析例子

--- app/src/main/java/com/kotlin/JsonTestKotlin.kt
+++ app/src/main/java/com/kotlin/JsonTestKotlin.kt
@@ -1,5 +1,8 @@
 package com.kotlin
 
+import com.google.gson.Gson
+import org.json.JSONObject
+
 /**
  * 单独的kt文件运行必须要新建一个kotlin项目再新建kt文件才能运行，在java项目中无法运行
  */
@@ -9,8 +12,37 @@ class JsonTestKotlin {
         @JvmStatic
         fun main(args: Array<String>) {
             println("Hello!")
+//            解析string类型的json字符串
+            /**
+             * 以下字节运行会抛出异常：
+             * Exception in thread "main" java.lang.RuntimeException: Stub!
+            at org.json.JSONObject.<init>(JSONObject.java:115)
+            at com.kotlin.JsonTestKotlin$Companion.main(JsonTestKotlin.kt:16)
+            at com.kotlin.JsonTestKotlin.main(JsonTestKotlin.kt)
+             */
+//            val jsonString = """{"name":"John","age":30,"city":"New York"}"""
+//            val jsonObject = JSONObject(jsonString)
+//
+//            val name = jsonObject.getString("name")
+//            val age = jsonObject.getInt("age")
+//            val city = jsonObject.getString("city")
+//
+//            println("Name: $name")
+//            println("Age: $age")
+//            println("City: $city")
+
+            /********************************/
+            val jsonString = """{"name":"John","age":30,"city":"New York"}"""
+            val gson = Gson()
+            val person = gson.fromJson(jsonString, Person::class.java)
+
+            println("Name: ${person.name}")
+            println("Age: ${person.age}")
+            println("City: ${person.city}")
         }
     }
+
+    class Person(val name: String, val age: Int, val city: String)
 }
 
 
